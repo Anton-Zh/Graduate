79c29b5a92505006e0ef3dfd07971c36f3c173a2
Anton Zh <fanat.iko88@gmail.com>
2019-01-29 20:49:27 +0300

    blur done

--- src/assets/scripts/modules/blur.js
+++ src/assets/scripts/modules/blur.js
@@ -1,10 +1,10 @@
 // var blur = (function () {
 //   var wrapper = document.querySelector('.js__blur__cont'),
-//       form = document.querySelector('..js__blur__form');
+//       form = document.querySelector('.js__blur');
 
 //   return {
 //     set: function () {
-//       var imgWidth = document.querySelector('.js__blur').offsetWidth, 
+//       var imgWidth = document.querySelector('.js__blur__form').offsetWidth, 
 //           posLeft = -wrapper.offsetLeft,
 //           posTop = -wrapper.offsetTop,
 //           blurCSS = form.style;
@@ -21,18 +21,26 @@
 //   blur.set();
 // }
 
-const container = document.querySelector('.js__blur');
-const sectionEl = document.querySelector('.js__blur__cont');
-const blurEl = document.createElement('.js__blur__form');
+
+const container = document.querySelector('.js__blur__cont');
+const sectionEl = document.querySelector('.js__blur');
+const blurEl = document.querySelector('.js__blur__form');
 
 let drawBlur = function () {
   const width = sectionEl.offsetWidth;
   const height = sectionEl.offsetHeight;
   const left = container.offsetLeft;
-  const bottom = height - (container.offsetTop + containeroffsetHeight);
+  const bottom = height - (container.offsetTop + container.offsetHeight);
 
-  blurEl.style.width = width +"px";
-  blurEl.style.height = height +"px";
-  blurEl.style.left = `${left}px`;
-  blurEl.style.bottom = `${bottom}px`;
+
+  blurEl.style.width = width + "px";
+  blurEl.style.height = height + "px";
+  blurEl.style.left = `-${left}px`;
+  blurEl.style.bottom = `-${bottom}px`;
+}
+
+drawBlur();
+
+window.onresize = function () {
+  drawBlur()
 }
